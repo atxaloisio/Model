d40642d68908ea460e598c9626378867249ee1af
Aloisio Xavier <atxaloisio@gmail.com>
2017-01-16 01:13:28 -0200

    salvando alterações

--- usuario.cs
+++ usuario.cs
@@ -17,7 +17,8 @@ namespace Model
         public int Id { get; set; }
         public string email { get; set; }
         public string nome { get; set; }
-        public string login { get; set; }
         public string password { get; set; }
+        public Nullable<System.DateTime> dtcriacao { get; set; }
+        public Nullable<System.DateTime> dtalteracao { get; set; }
     }
 }
